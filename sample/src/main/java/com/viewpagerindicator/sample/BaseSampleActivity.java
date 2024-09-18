20c829ced06a396a4e1d7c97b125417d7d62a490
harsewaksingh13 <harsewak.singh@healthera.co.uk>
2024-09-18 21:37:54 +0100

    buildable gralde updates

--- sample/src/main/java/com/viewpagerindicator/sample/BaseSampleActivity.java
+++ sample/src/main/java/com/viewpagerindicator/sample/BaseSampleActivity.java
@@ -10,6 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+
 public abstract class BaseSampleActivity extends FragmentActivity {
     private static final Random RANDOM = new Random();
 
@@ -25,26 +26,23 @@ public abstract class BaseSampleActivity extends FragmentActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.random:
-                final int page = RANDOM.nextInt(mAdapter.getCount());
-                Toast.makeText(this, "Changing to page " + page, Toast.LENGTH_SHORT);
-                mPager.setCurrentItem(page);
-                return true;
-
-            case R.id.add_page:
-                if (mAdapter.getCount() < 10) {
-                    mAdapter.setCount(mAdapter.getCount() + 1);
-                    mIndicator.notifyDataSetChanged();
-                }
-                return true;
-
-            case R.id.remove_page:
-                if (mAdapter.getCount() > 1) {
-                    mAdapter.setCount(mAdapter.getCount() - 1);
-                    mIndicator.notifyDataSetChanged();
-                }
-                return true;
+        if (item.getItemId() == R.id.random) {
+            final int page = RANDOM.nextInt(mAdapter.getCount());
+            Toast.makeText(this, "Changing to page " + page, Toast.LENGTH_SHORT).show();
+            mPager.setCurrentItem(page);
+            return true;
+        } else if (item.getItemId() == R.id.add_page) {
+            if (mAdapter.getCount() < 10) {
+                mAdapter.setCount(mAdapter.getCount() + 1);
+                mIndicator.notifyDataSetChanged();
+            }
+            return true;
+        } else if (item.getItemId() == R.id.remove_page) {
+            if (mAdapter.getCount() > 1) {
+                mAdapter.setCount(mAdapter.getCount() - 1);
+                mIndicator.notifyDataSetChanged();
+            }
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
